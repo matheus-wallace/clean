82fdc6e3250226af0f91461d87e7c265fd73ee3d
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-07-20 11:26:53 -0300

    feat: add `prisma service` to connect to communicate with database

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({ log: ['info', 'query', 'warn', 'error'] });
+  }
+  onModuleInit() {
+    return this.$connect();
+  }
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+  teste() {
+    return 'oi';
+  }
+}
